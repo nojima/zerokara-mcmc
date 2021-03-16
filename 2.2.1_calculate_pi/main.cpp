0b764013f54cfae3ae74dbec9a8534ef1e5b9071
Yusuke Nojima <nojima@ynojima.com>
2021-03-17 01:50:43 +0900

    Add plot

--- 2.2.1_calculate_pi/main.cpp
+++ 2.2.1_calculate_pi/main.cpp
@@ -1,13 +1,19 @@
+#include <Python.h>
 #include "lib/std.hpp"
 #include "lib/xoshiro256.hpp"
 #include <fmt/core.h>
+#include <matplotlibcpp.h>
 
-int main() {
-    Xoshiro256 rng;
+namespace plt = matplotlibcpp;
 
-    const size_t n_iterations = 100000;
-    size_t n_in = 0;
-    for(size_t iter = 1; iter <= n_iterations; ++iter) {
+Xoshiro256 rng;
+
+auto calculate_pi() {
+    vector<pair<int64_t, double>> result;
+
+    const int64_t n_iterations = 1000000;
+    int64_t n_in = 0;
+    for(int64_t iter = 1; iter <= n_iterations; ++iter) {
         auto x = rng.next_double();
         auto y = rng.next_double();
 
@@ -15,7 +21,29 @@ int main() {
 
         if(iter % 1000 == 0) {
             double approx_pi = 4.0 * (double)n_in / iter;
+            result.emplace_back(iter, approx_pi);
             fmt::print("{:8}: {:f}\n", iter, approx_pi);
         }
     }
+
+    return result;
+}
+
+void plot(const vector<pair<int64_t, double>>& result) {
+    vector<int64_t> xs;
+    vector<double> ys;
+    for(auto [x, y] : result) {
+        xs.emplace_back(x);
+        ys.emplace_back(y);
+    }
+
+    plt::figure_size(1200, 780);
+    plt::plot(xs, ys);
+    plt::title("Approximation of PI");
+    plt::save("./plot.png");
+}
+
+int main() {
+    auto result = calculate_pi();
+    plot(result);
 }
